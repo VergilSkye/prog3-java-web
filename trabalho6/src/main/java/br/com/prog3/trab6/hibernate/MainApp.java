946aeb77f53c1e4628fd82539b8f19337eaab0a3
Virgilio <virgiliosps@gmail.com>
2018-10-28 02:46:54 -0300

    Initial commit:tada:

---
+++ trabalho6/src/main/java/br/com/prog3/trab6/hibernate/MainApp.java
@@ -0,0 +1,24 @@
+package br.com.prog3.trab6.hibernate;
+
+import org.hibernate.Session;
+
+/**
+ * @author imssbora
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		// Check database version
+		String sql = "select version()";
+
+		String result = (String) session.createNativeQuery(sql).getSingleResult();
+		System.out.println(result);
+
+		session.getTransaction().commit();
+		session.close();
+
+		HibernateUtil.shutdown();
+	}
+}
